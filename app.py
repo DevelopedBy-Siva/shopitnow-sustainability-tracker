d1c7c35f3d318bb21555594bd7e7e8ce48aebc26
DevelopedBy-Siva <developedby.siva@gmail.com>
2025-11-02 01:23:21 -0700

    Setup for deployment

--- app.py
+++ app.py
@@ -6,11 +6,22 @@ from routes.carbon import carbon_bp
 from routes.user_tracking import user_bp
 from routes.recommend import recommend_bp
 from routes.summary import summary_bp
+import os
+
+
+def is_dev():
+    env = os.getenv("APP_ENV", "production")
+    if env == "development":
+        return True
+    return False
 
 
 def create_app():
     app = Flask(__name__)
-    # CORS(app, resources={r"/*": {"origins": "https://shopit-now-client.vercel.app"}})
+    if not is_dev():
+        CORS(
+            app, resources={r"/*": {"origins": "https://shopit-now-client.vercel.app"}}
+        )
     app.config.from_object(Config)
     db.init_app(app)
 
@@ -26,4 +37,10 @@ app = create_app()
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5001, debug=True)
+
+    if is_dev():
+        app.run(host="0.0.0.0", port=5001, debug=True)
+    else:
+        from waitress import serve
+
+        serve(app)
